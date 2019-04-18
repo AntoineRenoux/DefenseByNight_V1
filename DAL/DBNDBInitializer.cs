50a3312789b33d07db8384857d1a3758fad1139d
Antoine Renoux <ant.renoux@gmail.com>
2019-04-18 21:07:21 +0200

    Automapper + Autofac + Entity Framework

---
+++ DAL/DBNDBInitializer.cs
@@ -0,0 +1,30 @@
+﻿using DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class DBNDBInitializer : DropCreateDatabaseIfModelChanges<DBNContext>
+    {
+        protected override void Seed(DBNContext context)
+        {
+            var users = new List<User>
+            {
+                new User{Alias="Zesso", FirstName="Antoine", LastName="Renoux", BirthDate=new DateTime(1992, 2, 13), Email = "ant.renoux@gmail.com", Password = "5S5rtlEb$"},
+                new User{Alias="So", FirstName="Sophie", LastName="Vennin", BirthDate=new DateTime(1984, 2, 24), Email = "sophievennin@gmail.com", Password = "5S5rtlEb$"},
+                new User{Alias="B44", FirstName="Fabien", LastName="De Vienne", BirthDate=new DateTime(1991, 11, 2), Email = "fabien.devienne@gmail.com", Password = "5S5rtlEb$"}
+            };
+
+            users.ForEach(u =>
+            {
+                context.Users.Add(u);
+            });
+
+            context.SaveChanges();
+        }
+    }
+}
